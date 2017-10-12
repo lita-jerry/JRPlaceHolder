b36ee9f2a78bf33600c2e414a864f0aaf56f8200
ZhaoFeiK <zhaofei199210@gmail.com>
2017-10-12 14:34:34 +0800

    swizzle reloadData method in loadfunction to make sure every tabelView can execute jr_reloadData when inherit from a base tableView

--- JRTableViewPlaceHolder/UITableView+JRTableViewPlaceHolder.m
+++ JRTableViewPlaceHolder/UITableView+JRTableViewPlaceHolder.m
@@ -8,35 +8,31 @@
 
 @implementation UITableView (JRTableViewPlaceHolder)
 
++ (void)load {
+    Class class = [self class];
+    
+    SEL originalSelector = @selector(reloadData);
+    SEL swizzledSelector = @selector(jr_reloadData);
+    
+    Method originalMethod = class_getInstanceMethod(class, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+    
+    BOOL success = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+    if (success) {
+        class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+    } else {
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
+}
+
 - (void)jr_configureWithPlaceHolderBlock:(JRPlaceHolderBlock)block normalBlock:(JRPlaceHolderNormalBlock)normal{
     self.jr_placeHolderBlock = [block copy];
     if(self.jr_placeHolderBlock){
-        [self jrPlaceHolder_swizzling];
         self.jrPlaceHolder_configure = YES;
         self.jr_placeHolderNormalBlock = [normal copy];
         return;
     }
     self.jrPlaceHolder_configure = NO;
-}
-
-- (void)jrPlaceHolder_swizzling {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        
-        SEL originalSelector = @selector(reloadData);
-        SEL swizzledSelector = @selector(jr_reloadData);
-        
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-        
-        BOOL success = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
-        if (success) {
-            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
-    });
 }
 
 - (void)jr_reloadData {
